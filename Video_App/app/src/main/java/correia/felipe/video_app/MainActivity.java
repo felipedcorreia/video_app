72b5e4e30fd35981d0e6922277fb129536d27098
jjsafs <felipediasc@yahoo.com.br>
2017-11-24 18:07:18 -0200

    second commit

--- Video_App/app/src/main/java/correia/felipe/video_app/MainActivity.java
+++ Video_App/app/src/main/java/correia/felipe/video_app/MainActivity.java
@@ -1,27 +1,87 @@
 package correia.felipe.video_app;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.net.Uri;
+import android.os.Handler;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
+import android.widget.VideoView;
 
 import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
-    private final int VIDEO_REQUEST_CODE = 100;
+    private int VIDEO_REQUEST_CODE = 100;
+    private int START_CAMERA = 0;
+    Button brecord, bplay;
+    VideoView vv;
+    private boolean bVideoIsBeingTouched = false;
+    private Handler mHandler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        brecord = (Button) findViewById(R.id.brecord);
+        bplay = (Button) findViewById(R.id.bplay);
+        vv = (VideoView) findViewById(R.id.videoView);
+
+
+        brecord.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent callVideo = new Intent();
+                callVideo.setAction(MediaStore.ACTION_VIDEO_CAPTURE);
+                startActivityForResult(callVideo, START_CAMERA);
+
+            }
+        });
+
+        vv.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                mp.setLooping(true);
+            }
+        });
+
+        vv.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                if (!bVideoIsBeingTouched) {
+                    bVideoIsBeingTouched = true;
+                    vv.requestFocus();
+                    
+                    mHandler.postDelayed(new Runnable() {
+                        public void run() {
+                            bVideoIsBeingTouched = false;
+                        }
+                    }, 100);
+                }
+
+                return true;
+            }
+        });
+
+        bplay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 
-    public void captureVideo(View view){
+
+
+
+    public void captureVideo(){
         Intent camera_intent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
         File video_file = getFilepath();
         Uri video_uri = Uri.fromFile(video_file);
@@ -32,13 +92,14 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if(requestCode==VIDEO_REQUEST_CODE){
-            if(resultCode==RESULT_OK){
-                Toast.makeText(getApplicationContext(), "Sucesso", Toast.LENGTH_SHORT).show();
+        if(requestCode==START_CAMERA && resultCode==RESULT_OK){
+                Uri videoUri = data.getData();
+                vv.setVideoURI(videoUri);
+                vv.start();
             }else {
                 Toast.makeText(getApplicationContext(), "Opsss.....", Toast.LENGTH_SHORT).show();
             }
-        }
+
     }
 
     public File getFilepath(){
